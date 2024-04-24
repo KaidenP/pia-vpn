bc2d2f338efac5403b71330e9e4dc654713a442d
Kaiden <me@kaidenprince.com>
2024-04-24 01:49:25 -0400

    Initial Commit

---
+++ src/pia.js
@@ -0,0 +1,223 @@
+#!/usr/bin/env node
+
+import child_process from 'node:child_process'
+import {promisify} from  'node:util'
+const exec = promisify(child_process.exec)
+import fs from 'node:fs/promises'
+import got from 'got'
+import FormData from "form-data"
+import Docker from "dockerode"
+import os from 'os'
+
+const docker = new Docker({socketPath: '/var/run/docker.sock'})
+let qbt
+
+if (!process.env.PIA_USERNAME || process.env.PIA_USERNAME === '') throw new Error("Missing PIA_USERNAME environment variable")
+if (!process.env.PIA_PASSWORD || process.env.PIA_PASSWORD === '') throw new Error("Missing PIA_PASSWORD environment variable")
+if (!process.env.DATA_DIR || process.env.DATA_DIR === '') throw new Error("Missing DATA_DIR environment variable")
+
+const loginData = new FormData()
+loginData.append('username', process.env.PIA_USERNAME)
+loginData.append('password', process.env.PIA_PASSWORD)
+
+const {token} = await got.post('https://www.privateinternetaccess.com/api/client/v2/token', {
+    body: loginData
+}).json();
+
+console.log(`Got token : ${token.substring(0,5)}*****`);
+
+async function cleanup() {
+    console.log("Cleaning up...")
+    if (qbt) {
+        console.log("Removing qbt container...")
+        await qbt.remove({force:true})
+    }
+    console.log("Cleaning up... Done!")
+}
+
+async function startQbt(port) {
+    process.stdout.write('Pulling qbittorent docker image')
+    await docker.pull('linuxserver/qbittorrent:latest', function(err, stream) {
+        //...
+        docker.modem.followProgress(stream, onFinished, onProgress);
+
+        async function onFinished(err, output) {
+            console.log(' Done!')
+            console.log("Starting qbt...")
+            qbt = await docker.createContainer( {
+                Image: 'linuxserver/qbittorrent:latest',
+                AttachStdout: true,
+                AttachStderr: true,
+                HostConfig: {
+                    NetworkMode: `container:${os.hostname()}`,
+                    AutoRemove: true,
+                    Binds: [
+                        `${process.env.DATA_DIR}:/downloads`,
+                        `${process.env.DATA_DIR}/.config:/config`
+                    ]
+                },
+                name: `qbt-${os.hostname()}`,
+                Env: ["PUID=10000", "PGID=10000", "TZ=America/Toronto", `TORRENTING_PORT=${port}`]
+            })
+            await qbt.start()
+            qbt.attach({stream: true, stdout: true, stderr: true}, function (err, stream) {
+                stream.pipe(process.stdout);
+            })
+            console.log("Starting qbt... DONE!")
+        }
+        function onProgress(event) {
+            process.stdout.write(".");
+        }
+    })
+}
+
+async function portForward(ip, hostname, payload, signature) {
+    let pf_json = await got.get(`https://${ip}:19999/bindPort?${(new URLSearchParams({
+        signature: signature,
+        payload: payload
+    })).toString()}`, {
+        headers: {
+            Host: hostname
+        },
+        https: {
+            certificateAuthority: await fs.readFile('ca.rsa.4096.crt')
+        }
+    }).json()
+
+    if (pf_json.status !== 'OK') throw new Error('Port Refresh Failed')
+
+    console.log("Doing a ping test")
+    try {
+        await exec('ping -c 5 8.8.8.8')
+    } catch (e) {
+        throw new Error('Ping test failed')
+    }
+    console.log("Done!")
+}
+async function connect(ip, hostname, DIP=false) {
+    console.log('Generating Keys...')
+    const privkey = (await exec(`wg genkey`)).stdout
+    const pubkey = (await exec(`echo '${privkey}' | wg pubkey`)).stdout
+
+    console.log(`Registering keys with PIA API...`)
+    let pia_json
+    if (DIP) {
+        pia_json = await got.get(`https://${ip}:1337/addKey?${(new URLSearchParams({
+            pubkey: pubkey
+        })).toString()}`, {
+            headers: {
+                Host: hostname
+            },
+            https: {
+                certificateAuthority: await fs.readFile('ca.rsa.4096.crt')
+            },
+            username: `dedicated_ip_${DIP.dip_token}`,
+            password: ip
+        }).json()
+    } else {
+        pia_json = await got.get(`https://${ip}:1337/addKey?${(new URLSearchParams({
+            pubkey: pubkey,
+            pt: token
+        })).toString()}`, {
+            headers: {
+                Host: hostname
+            },
+            https: {
+                certificateAuthority: await fs.readFile('ca.rsa.4096.crt')
+            }
+        }).json()
+    }
+    if (pia_json.status !== 'OK') throw new Error("Server did not return OK. Stopping now.")
+
+    console.log(`Connecting to ${hostname}...`)
+
+    console.log('Setting up Firewall...')
+    await exec('wg-quick down pia || true')
+    await exec('ip6tables -P INPUT DROP && ip6tables -P OUTPUT DROP && ip6tables -P FORWARD DROP')
+    await exec('iptables -P INPUT DROP && iptables -P OUTPUT DROP && iptables -P FORWARD DROP')
+    await exec('iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT')
+    await exec('iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT')
+    await exec('iptables -A OUTPUT -o lo -j ACCEPT && iptables -A INPUT -i lo -j ACCEPT')
+    await exec('iptables -A OUTPUT -o pia -j ACCEPT')
+    await exec(`iptables -A OUTPUT -p udp --dport ${pia_json.server_port} -d ${ip} -o eth0 -j ACCEPT`)
+    await exec(`iptables -A INPUT -p tcp -i eth0 --dport 8080 -j ACCEPT`)
+    console.log('Setting up Firewall... Done!')
+
+    await fs.writeFile('/etc/wireguard/pia.conf', `[Interface]
+Address = ${pia_json.peer_ip}
+PrivateKey = ${privkey}
+DNS = ${pia_json.dns_servers[0]}
+PostUp = ip rule add from $(ip a show dev eth0 | grep 'inet ' | awk '{ print $2; }' | sed -r 's@/[0-9]+$@@') table main
+PostUp = iptables -I OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -m conntrack ! --ctstate ESTABLISHED,RELATED -j REJECT
+PreDown = ip rule del from $(ip a show dev eth0 | grep 'inet ' | awk '{ print $2; }' | sed -r 's@/[0-9]+$@@') table main
+PreDown = iptables -D OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -m conntrack ! --ctstate ESTABLISHED,RELATED -j REJECT
+[Peer]
+PersistentKeepalive = 25
+PublicKey = ${pia_json.server_key}
+AllowedIPs = 0.0.0.0/0
+Endpoint = ${ip}:${pia_json.server_port}
+`)
+    console.log(`Starting VPN...`)
+    console.log(`${(await exec('wg-quick up pia')).stderr}`)
+    console.log(`Done!`)
+
+
+    console.log(`Setup Port Forwarding @${ip}...`)
+    let pf_json = await got.get(`https://${ip}:19999/getSignature?${(new URLSearchParams({
+        token: token
+    })).toString()}`, {
+        headers: {
+            Host: hostname
+        },
+        https: {
+            certificateAuthority: await fs.readFile('ca.rsa.4096.crt')
+        }
+    }).json()
+    if (pf_json.status !== 'OK') throw new Error('Error Registering Port')
+    let payload = JSON.parse(atob(pf_json.payload))
+    console.log(`We got port ${payload.port}`)
+    await exec(`iptables -A INPUT -p udp -i pia --dport ${payload.port} -j ACCEPT`)
+    await exec(`iptables -A INPUT -p tcp -i pia --dport ${payload.port} -j ACCEPT`)
+
+    setInterval(portForward.bind(undefined, [ip, hostname, pf_json.payload, pf_json.signature]), 15*60*1000)
+    await portForward(ip, hostname, pf_json.payload, pf_json.signature)
+    await startQbt(payload.port)
+}
+
+let DIP
+if (!process.env.PIA_DIP || process.env.PIA_DIP === '') {
+    console.log("Not using DIP.")
+    DIP=false
+} else {
+    DIP = (await got.post('https://www.privateinternetaccess.com/api/client/v2/dedicated_ip',{
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Token ${token}`
+        },
+        body: JSON.stringify({ tokens: [process.env.PIA_DIP] })
+    }).json())[0]
+
+    if (DIP.status !== 'active') throw new Error(`Got bad response: ${JSON.stringify(DIP)}`)
+    await connect(DIP.ip, DIP.cn, DIP)
+}
+
+const trap = ['exit', 'SIGINT', 'SIGTERM']
+function exitHandler(signal="unknown") {
+    process.stdin.resume()
+
+    console.log(`Exiting due to "${signal}"`)
+
+    trap.forEach(sig=>{
+        process.removeListener(sig, exitHandler)
+    })
+    cleanup().then(()=>process.exit())
+}
+
+trap.forEach(sig=>{
+    process.once(sig, exitHandler)
+})
+process.on('uncaughtException', async function (err) {
+    console.error(err.stack)
+    exitHandler()
+    process.exit(1)
+})
